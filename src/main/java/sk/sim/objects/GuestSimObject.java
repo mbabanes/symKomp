8b19ad83e75e531502d92fbc8e980acc6602e08a
scyzoryk03@gmail.com <scyzoryk03@gmail.com>
2018-05-02 14:27:07 +0200

    Change out operation

--- src/main/java/sk/sim/objects/GuestSimObject.java
+++ src/main/java/sk/sim/objects/GuestSimObject.java
@@ -1,41 +1,34 @@
-package sk.sim.object;
+package sk.sim.objects;
 
-import deskit.SimActivity;
-import deskit.SimManager;
 import deskit.SimObject;
 import lombok.Getter;
-import sk.sim.activity.GuestOutingActivity;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Getter
 public class GuestSimObject extends SimObject
 {
-
     private static AtomicInteger ID = new AtomicInteger();
 
     private int id = ID.getAndIncrement();
-
     private WaiterSimObject waiterSimObject;
-
     private int time;
+
 
     public GuestSimObject(int time)
     {
         this.time = time;
     }
 
-    public void setWaiterSimObject(WaiterSimObject waiterSimObject)
+    public void setWaiter(WaiterSimObject waiterSimObject)
     {
         this.waiterSimObject = waiterSimObject;
     }
 
     public void out()
     {
-        SimActivity outing = new GuestOutingActivity(this);
-
-        System.out.println("wychodzi gosc: " + id + " po czasie " + time);
-        SimActivity.callActivity(this, outing);
+        System.out.println("Gość[" + id + "]: Wychodzi (" + waiterSimObject.debugMessage() + ")");
+        waiterSimObject.getCurrentGuestNumber().decrementAndGet();
     }
 
     @Override
@@ -44,5 +37,11 @@ public class GuestSimObject extends SimObject
         return "GuestSimObject{" +
                 "id=" + id +
                 '}';
+    }
+
+
+    public String debugMessage()
+    {
+        return "[Gość " + id + "]: ";
     }
 }
