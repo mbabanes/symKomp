b8a7b2c9af8950de8a0f1516e5e70cc1300665c8
scyzoryk03@gmail.com <scyzoryk03@gmail.com>
2018-05-02 14:19:05 +0200

    New Concept of new guest comming,
    refactoring

--- src/main/java/sk/sim/activities/NewGuestComingActivity.java
+++ src/main/java/sk/sim/activities/NewGuestComingActivity.java
@@ -1,19 +1,19 @@
-package sk.sim.activity;
+package sk.sim.activities;
 
 import deskit.SimActivity;
 import sk.sim.RestaurantSimObject;
-import sk.sim.exception.NoFreeWaiterException;
-import sk.sim.object.GuestSimObject;
+import sk.sim.objects.GuestSimObject;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class NewGuestActivity extends SimActivity
+public class NewGuestComingActivity extends SimActivity
 {
     public static final int MAX_GUEST_NUMBER = 20;
-    static Random random = new Random();
+    private static Random random = new Random();
 
     private AtomicInteger counter = new AtomicInteger();
+
 
     @Override
     public void action()
@@ -26,17 +26,17 @@ public class NewGuestActivity extends SimActivity
             {
                 System.out.println("\nNowi goscie stop\n");
                 waitDuration(10000);
-                callWaiters();
             }
 
 
-            if ((counter.getAndIncrement() > MAX_GUEST_NUMBER))
+            if ( counter.getAndIncrement() > MAX_GUEST_NUMBER )
             {
-                while(RestaurantSimObject.expectantGuestAreInRestaurant())
+                while( RestaurantSimObject.expectantGuestAreInRestaurant() )
                 {
                     waitDuration(1000);
-                    callWaiters();
                 }
+
+                RestaurantSimObject.close();
                 break;
             }
         }
@@ -47,17 +47,5 @@ public class NewGuestActivity extends SimActivity
         GuestSimObject guest = new GuestSimObject(random.nextInt(2300));
         RestaurantSimObject.expectantGuests.addLast(guest);
         System.out.println("Nowy gosc w kolejce: " + guest);
-    }
-
-    private void callWaiters()
-    {
-        try
-        {
-            RestaurantSimObject.callWaiters();
-        }
-        catch (NoFreeWaiterException e)
-        {
-            RestaurantSimObject.bidFarewellSomeGuest();
-        }
     }
 }
