c9a72dfa8b1c9debb0674f7eaa897780d11a96d7
scyzoryk03@gmail.com <scyzoryk03@gmail.com>
2018-05-11 16:36:06 +0200

    Adding Lombok Getter annotation. Adding order id. Adding relation with guest.
    Adding debug message

---
+++ src/main/java/sk/sim/objects/OrderSimObject.java
@@ -0,0 +1,71 @@
+package sk.sim.objects;
+
+import deskit.SimObject;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Getter
+public class OrderSimObject extends SimObject
+{
+    private static AtomicInteger ID = new AtomicInteger(0);
+
+    private int id;
+
+    private List<Meal> meals = new ArrayList<>();
+    private List<Drink> drinks = new ArrayList<>();
+
+    private GuestSimObject guestSimObject;
+
+    private long preparingTime;
+
+    public OrderSimObject()
+    {
+        this.id = ID.getAndIncrement();
+    }
+
+    public OrderSimObject(GuestSimObject guestSimObject)
+    {
+        this();
+        this.guestSimObject = guestSimObject;
+    }
+
+    public void addMeal(Meal meal)
+    {
+        meals.add(meal);
+
+        preparingTime += meal.getPreparingTime();
+        meal.increment();
+
+    }
+
+    public void addDrink(Drink drink)
+    {
+        drinks.add(drink);
+        preparingTime += drink.getPreparingTime();
+        drink.increment();
+    }
+
+
+    public int getMealsNumber()
+    {
+        return meals.size();
+    }
+
+    public int getDrinksNumber()
+    {
+        return drinks.size();
+    }
+
+    public void setGuest(GuestSimObject guest)
+    {
+        this.guestSimObject = guest;
+    }
+
+    public String debugMessage()
+    {
+        return "[Zamowienie " + id + "]: ";
+    }
+}
