33e3e5426d66fb5cd34adf5f341fd8e907d41a68
scyzoryk03@gmail.com <scyzoryk03@gmail.com>
2018-05-15 18:29:47 +0200

    Adding tables (used only for visualisation).
    Refactor make constant MAX_GUEST

--- src/main/java/sk/sim/objects/WaiterSimObject.java
+++ src/main/java/sk/sim/objects/WaiterSimObject.java
@@ -3,13 +3,18 @@ package sk.sim.objects;
 import deskit.SimObject;
 import lombok.Getter;
 import lombok.Setter;
+import sk.sim.gui.visualisation.object.Table;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Setter
 @Getter
 public class WaiterSimObject extends SimObject
 {
+    private static final int MAX_GUEST = 3;
+
     private static AtomicInteger ID = new AtomicInteger();
 
     private int id = ID.getAndIncrement();
@@ -18,9 +23,21 @@ public class WaiterSimObject extends SimObject
 
     private AtomicInteger currentGuestNumber = new AtomicInteger();
 
+
+    private List<Table> tables = new ArrayList<>();
+
+
+    public WaiterSimObject()
+    {
+        for (int i = 0; i < MAX_GUEST; i++)
+        {
+            tables.add(new Table());
+        }
+    }
+
     public boolean isFree()
     {
-        if (currentGuestNumber.get() == 3)
+        if (currentGuestNumber.get() == MAX_GUEST)
             return false;
         return true;
     }
