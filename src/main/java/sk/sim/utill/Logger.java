70229360c216600f2abc6c79ee3ac5a7b37bdbf4
scyzoryk03@gmail.com <scyzoryk03@gmail.com>
2018-05-26 14:47:08 +0200

    Make logger not in real time. Log is doing in memory then it is possible to print in in textArea.

--- src/main/java/sk/sim/utill/Logger.java
+++ src/main/java/sk/sim/utill/Logger.java
@@ -1,13 +1,31 @@
 package sk.sim.utill;
 
-import javafx.scene.control.TextArea;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.function.Supplier;
 
 public class Logger
 {
-    public static TextArea loggerr;
+    private static Deque<String> log = new LinkedList<>();
 
     public static void log(String message)
     {
-        loggerr.appendText(message + '\n');
+//        logger.appendText(message + '\n');
+        log.addLast(message + '\n');
+    }
+
+    public static void log(Supplier<String> messageSup)
+    {
+        log(messageSup.get());
+    }
+
+    public static void consoleLog(Supplier<String> messageSup)
+    {
+//        System.out.println(messageSup.get());
+    }
+
+    public static Deque<String> getLog()
+    {
+        return log;
     }
 }
