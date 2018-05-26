e7fc6e081d0767e8ef2b950172b111d78ab66098
scyzoryk03@gmail.com <scyzoryk03@gmail.com>
2018-05-26 14:36:03 +0200

    ANEw method of bringing order to guest. Waiter hasnt be busy.

--- src/main/java/sk/sim/activities/waiters/OrderRealizationActivity.java
+++ src/main/java/sk/sim/activities/waiters/OrderRealizationActivity.java
@@ -5,6 +5,7 @@ import deskit.synchronizers.Semaphore;
 import sk.sim.activities.cook.OrderNote;
 import sk.sim.activities.cook.OrderQueue;
 import sk.sim.objects.OrderSimObject;
+import sk.sim.objects.WaiterSimObject;
 import sk.sim.utill.Logger;
 
 public class OrderRealizationActivity extends SimActivity
@@ -26,11 +27,30 @@ public class OrderRealizationActivity extends SimActivity
         OrderNote orderNote = new OrderNote(order, semaphore);
 
         OrderQueue.addOrder(orderNote);
-        Logger.log(order.debugMessage() +   "Przekazane do kolejki realizacji");
+        Logger.log(() -> order.debugMessage() +   "Przekazane do kolejki realizacji");
+
+        WaiterSimObject waiter = order.getGuestSimObject().getWaiterSimObject();
+        waiter.setBusy(false);
+
         semaphore.wait(this);
         waitOnSemaphore(semaphore);
-        Logger.log(order.debugMessage() +  "Gotowe");
-        callActivity(order.getGuestSimObject().getWaiterSimObject(), new BringingOrderForGuestActivity(order, this.semaphore));
-        Logger.log(order.debugMessage() +   "Zamowienie zrealizowane");
+        Logger.log(() -> order.debugMessage() +  "Gotowe");
+
+        bringToGuest(waiter);
+        Logger.log(  () -> order.debugMessage() +   "Zamowienie zrealizowane" );
+
+    }
+
+    private void bringToGuest(WaiterSimObject waiter)
+    {
+        while (waiter.isBusy())
+        {
+            waitDuration(1000);
+            Logger.consoleLog(() -> waiter.debugMessage() + "zbyt zajety");
+        }
+//        Logger.consoleLog( () -> waiter.debugMessage() + " zanosi jedzenie");
+        waiter.setBusy(true);
+
+        callActivity(waiter, new BringingOrderForGuestActivity(order, this.semaphore));
     }
 }
